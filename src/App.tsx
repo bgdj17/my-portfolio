9cbdf2f5d0069303ccf7284c73fb49ed4fc70c6a
Bruna G de Jesus <bgdj17@gmail.com>
2024-03-15 16:32:54 -0300

    add sass and home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Home from './pages/Home'
 import './App.css'
 
 function App() {
   return (
     <>
-     <div><h1>teste Git Action</h1></div>
+      <div>
+        <Home></Home>
+        
+      </div>
     </>
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import './Home.scss'
+
+function Home() {
+    return (
+        <>
+            <div className='content'>
+                <h1>Hi,</h1>
+                <h1>I'm Bruna,</h1>
+                <h1>web developer</h1>
+            </div>
+        </>
+    )
+}
+
+export default Home
+
